e21aaff5670b72f29987b0b10a027e177ac44d4a
Ned Fellenor <nedfellenor@gmail.com>
2020-01-09 18:54:29 +0000

    Created unit_tests.js

---
+++ Unit_Tests.js
@@ -0,0 +1,10 @@
+//test to check GET request for plant
+//test for login 
+
+var mocha = require("mocha");
+var chai = require("chai");
+var listener = require("./Server.js"); //Require listener variable from Server
+
+
+
+
