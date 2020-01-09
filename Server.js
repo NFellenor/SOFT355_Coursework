f3a7ef5af74c7f4f2e8698b7b0e171406c932987
Ned Fellenor <nedfellenor@gmail.com>
2020-01-09 17:43:19 +0000

    Sorted packages, edited HTML and Server.js

--- Server.js
+++ Server.js
@@ -3,35 +3,26 @@ var mongoose = require("mongoose");
 var db = require("./db.js");
 var schema = require("./Schema.js");
 
-var uri = "mongodb+srv://DbUserNF:<password>@soft355-dgfnc.mongodb.net/test?retryWrites=true&w=majority"; //Mongo DB URI
-
 var app = express();
+var uri = "mongodb+srv://DbUserNF:MSDOSislife50@soft355-dgfnc.mongodb.net/test?retryWrites=true&w=majority"; //Mongo DB URI
+
+mongoose.Promise = global.Promise;
 
 app.listen('9000', function(){
     mongoose.connect(uri, {useNewUrlParser: true, useUnifiedTopology: true}).then((test) => {
     console.log("Connected to DB");
+    console.log("Listening on 9000");
+    });
 });
 
 app.use(express("resources"));
 
-app.get("/plant/:", function(request, response) { //Edit
-    db.getCard(request.params.id).then(function(card) {
-        response.redirect("/" + card.filename);
+app.get("/plant", function(request, response){
+    db.getPlant(request.id).then(function(plant) {
+        response.send(plant);
+        console.log("GET Plant finished");
     });
 });
-
-app.get("/newgame", function(request, response) { //Edit 
-    logic.newGame().then(function(game) {
-        console.log("created a new game: " + game._id);
-        response.contentType("application/json");
-        response.send({"gameId": game._id});
-    });
-})
-
-    // Some output for the interested reader...
-    console.log("Listening on 9000");
-})
-
 
 //google ajax and jquery to get onto html page
 //api endpoint to pass data from server to client 
--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 var Schema = require("./Schema");
 
 async function getPlant(id) {
-    return await schemas.Plant.findOne({"id": id}); //Search schema file with associated parameters
-}
+    return await Schema.Plant.findOne({"id": id}); //Search schema file with associated parameters
+};
 
 module.exports.getPlant = getPlant;
